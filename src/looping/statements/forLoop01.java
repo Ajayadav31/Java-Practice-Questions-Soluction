7f0fd21b16ec65a9b6739b3418ca48fd2fc19e46
akashydv311 <akashydv311@gmail.com>
2023-07-24 11:33:12 +0530

    Looping start pattten

---
+++ src/looping/statements/forLoop01.java
@@ -0,0 +1,75 @@
+package looping.statements;
+
+public class forLoop01 {
+	public static void main(String[] args) {
+		
+/*
+ * * * * *
+ * * * *
+ * * *
+ * *
+ * 
+ */
+		
+		for(int i=5; i>0; i--) {
+			
+			for(int j=i; j>0; j--) {
+				
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		
+/*
+ * 
+ * *
+ * * *
+ * * * *
+ * * * * *
+ */
+		System.out.println("\n");
+
+		for(int i=0; i<5; i++) {
+			
+			for(int j=0; j<=i; j++) {
+				
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+	/*
+	 
+ * * * * *
+ * * * *
+ * * *
+ * *
+ * 
+ * *
+ * * *
+ * * * *
+ * * * * *
+ 
+	 */
+		
+		System.out.println("\n\n\n\n");
+		
+		for(int i=10; i>0; i--) {
+			
+			for(int j=i; j>0; j--) {
+				
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=1; i<10; i++) {
+			
+			for(int j=0; j<=i; j++) {
+				
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
