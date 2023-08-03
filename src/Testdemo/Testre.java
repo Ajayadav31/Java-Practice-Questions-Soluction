88eab87fb43cff12034afc625a98f2ef52d02a51
Ajay <ajayadav3108@gmail.com>
2023-08-03 11:38:31 +0530

    Testing local environment

---
+++ src/Testdemo/Testre.java
@@ -0,0 +1,8 @@
+package Testdemo;
+
+public class Testre {
+	public static void main(String[] args) {
+		 System.out.println("Hello");
+	}
+
+}
--- src/UseLaptop.java
+++ src/UseLaptop.java
@@ -1,4 +1,4 @@
-package setter.getter;
+
 
 class Laptop{
 	
