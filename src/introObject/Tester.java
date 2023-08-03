8ae86d9b3b15db017c6de907bf9ef709bc5567ba
akashydv311 <akashydv311@gmail.com>
2023-08-03 12:25:49 +0530

    Adding two numbers

--- src/introObject/Tester.java
+++ src/introObject/Tester.java
@@ -1,5 +1,27 @@
 package introObject;
 
-public class Tester {
+class Myprograms{
+	
+	// properitys
+	
+	// methods
+	
+	public void addTwoNumbers(int num1, int num2) {
+		
+		int result = num1 + num2;
+		System.out.println("Sum of 2 numbers is: "+ result);
+		
+	}
+}
 
+
+
+public class Tester {
+	public static void main(String[] args) {
+		
+		Myprograms programs = new Myprograms();
+		
+		programs.addTwoNumbers(12, 56);
+		
+	}
 }
