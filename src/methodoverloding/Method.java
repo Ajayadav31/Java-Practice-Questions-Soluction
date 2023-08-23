fcbcf133e4a8b51718fedef75f9e16888836b241
akashydv311 <akashydv311@gmail.com>
2023-08-23 12:26:54 +0530

    Method overloding added

---
+++ src/methodoverloding/Method.java
@@ -0,0 +1,25 @@
+package methodoverloding;
+
+public class Method {
+	
+//	private int num1 = 10;
+//	private int num2 = 45;
+	
+	private int num1;
+	private int num2;
+	
+	// setter getter
+	public void setNum1(int x) {
+		this.num1 = x;
+	}
+	public void setNum2(int y) {
+		this.num2 = y;
+	}
+	
+	public int getNum1() {
+		return this.num1;
+	}
+	public int getNum2() {
+		return this.num2;
+	}
+}
