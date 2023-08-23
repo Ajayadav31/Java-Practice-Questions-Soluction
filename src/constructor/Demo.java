d90f1ec85203a56ec381469f016834d3b8e673a3
akashydv311 <akashydv311@gmail.com>
2023-08-23 14:17:07 +0530

    Constructor added

---
+++ src/constructor/Demo.java
@@ -0,0 +1,11 @@
+package constructor;
+
+public class Demo {
+	
+	// default cons
+	
+	// non - perametrized cons
+	public Demo() {
+		System.out.println("msg");
+	}
+}	
--- src/demo1/Main.java
+++ src/demo1/Main.java
@@ -1,7 +1,11 @@
 package demo1;
 
+import constructor.Demo;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
+		
+		Demo demo = new Demo();
 	}
 }
